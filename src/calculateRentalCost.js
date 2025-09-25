8be560ac888693a3b77cd61b5030ae855395a49c
Josias <josiasonesokataborda22@gmail.com>
2025-09-25 10:54:14 -0300

    resolved task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const minDaysDescont = 3;
+  const maxDaysDescont = 7;
+  const descontThreeDays = 20;
+  const descontSevenDays = 50;
+  const dayPrice = 40;
+  const totalPrice = dayPrice * days;
+
+  if (days >= maxDaysDescont) {
+    return totalPrice - descontSevenDays;
+  }
+
+  if (days >= minDaysDescont) {
+    return totalPrice - descontThreeDays;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
